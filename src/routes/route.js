d1ff3924f8042eaf91ad04bf3d298067b1147269
Praveenkumarpravin <ppalraj361@gmail.com>
2025-06-24 13:09:28 +0530

    routes added for order Items

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { GetOrderItems, CreateOrderItems, UpdateOrderItems, DeleteOrderItems } = require("../controller/orderItems");
-const { GetOrderStatuses } = require("../controller/orderStatuses");
 const router = express.Router();
 
 //user table
-router.get("/users",getUser);
-router.post("/users",createUser);
-router.patch("/users/:id",updateUser);
-router.delete("/users/:id",deleteUser);
+router.get("/users", getUser);
+router.post("/users", createUser);
+router.patch("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
+// order Items
+router.get("/orderItems", GetOrderItems);
+router.post("/orderItems", CreateOrderItems);
+router.put("/orderItems/:id", UpdateOrderItems);
+router.delete("/orderItems/:id", DeleteOrderItems);
 
 module.exports = router;
