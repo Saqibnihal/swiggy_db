161639f5cfa996f46187bc01a5f38bc86193290e
Praveenkumarpravin <ppalraj361@gmail.com>
2025-06-24 12:37:25 +0530

    Order Items controller added and added route for order Items controller

---
+++ src/controller/orderItems.js
@@ -0,0 +1,53 @@
+
+const orderItems = require('../model/orderItems')
+
+exports.GetOrderItems = async (req, res) => {
+    try {
+        const orderData = await orderItems.query();
+        res.status(200).json({ message: "Successfully Fetched Order Items", orderData })
+    } catch (error) {
+        res.status(500).json({ message: "Error in Fetching Order Items", error: error.message })
+    }
+}
+
+exports.CreateOrderItems = async (req, res) => {
+    const orderData = req.body;
+    if (!orderData.orderId || !orderData.menuItemId || !orderData.quantity || !orderData.itemPrice || !orderData.total) {
+        res.status(400).json({ message: 'Fill all the fields' })
+    }
+    try {
+        const datas = await orderItems.query().insert(orderData)
+        res.status(200).json({ message: 'Successfully Created Order Items', datas })
+    } catch (error) {
+        res.status(500).json({ message: "Error in Creating Order Items", error: error.message })
+    }
+}
+
+exports.UpdateOrderItems = async (req, res) => {
+    const { id } = req.params;
+    const updatedData = req.body;
+    try {
+        const updated = await orderItems.query().patchAndFetchById(id, updatedData);
+        if (!updated) {
+            return res.status(404).json({ message: 'Order Item not found' });
+        }
+        res.status(200).json({ message: 'Successfully Updated Order Item', updated });
+    } catch (error) {
+        res.status(500).json({ message: "Error in Updating Order Item", error: error.message });
+    }
+};
+
+exports.DeleteOrderItems = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleted = await orderItems.query().deleteById(id);
+
+        if (deleted === 0) {
+            return res.status(404).json({ message: 'Order Item not found' });
+        }
+
+        res.status(200).json({ message: 'Successfully Deleted Order Item' });
+    } catch (error) {
+        res.status(500).json({ message: "Error in Deleting Order Item", error: error.message });
+    }
+};
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const { GetOrderItems, CreateOrderItems, UpdateOrderItems, DeleteOrderItems } = require("../controller/orderItems");
+const { GetOrderStatuses } = require("../controller/orderStatuses");
 const router = express.Router();
 
-router.get("");
+// praveen
+router.get("/orderItems", GetOrderItems);
+router.post("/orderItems", CreateOrderItems);
+router.put("/orderItems/:id", UpdateOrderItems);
+router.delete("/orderItems/:id", DeleteOrderItems);
+
+router.get("/orderstatus", GetOrderStatuses);
+router.post("/orderstatus", GetOrderStatuses);
+router.put("/orderstatus/:id", GetOrderStatuses);
+router.delete("/orderstatus/:id", GetOrderStatuses);
+
 
 module.exports = router;
